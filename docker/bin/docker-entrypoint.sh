b96130abeab7183d4bb4a41c1e3bddd65bb4cf04
andrauss <fernandoandrauss@hotmail.com>
2024-05-24 00:10:47 -0300

    FIX: fix infrastructure k8s and Docker

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -1,10 +1,3 @@
-#echo "start node server"
-#echo "node /var/www/dist/src/index.js"
-#node /var/www/dist/index.js
-npm install
-
-echo "wait postgres db server"
-dockerize -wait tcp://pedidos-ms-postgres:5432 -timeout 20s
-
 echo "start node server"
-node index.js
+echo "node /var/www/dist/src/index.js"
+node /var/www/dist/index.js
